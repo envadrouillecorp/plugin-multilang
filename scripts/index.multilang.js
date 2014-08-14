b8c714493a330daaf47e7db658baad5fbba54884
Baptiste <baptiste.lepers@gmail.com>
2014-08-13 22:04:38 -0700

    Don't always upload the complex script in description

--- scripts/index.multilang.js
+++ scripts/index.multilang.js
@@ -4,14 +4,25 @@ var MultilangPlugin = {
    preWriteJson:function(id, dir, cb) {
       var uid = MultilangPlugin.uid(id);
       var div = uid+'_content'
+      var nb_non_empty = 0;
+      var first_content = '';
 
       var content = '<div id="'+div+'" class="customtranslate"></div>';
-      for(var i in MultilangPlugin.langs)
-         if($('#multid_'+id+'_'+i).val() != '')
+      for(var i in MultilangPlugin.langs) {
+         if($('#multid_'+id+'_'+i).val() != '') {
+            nb_non_empty++;
+            if(!first_content)
+               first_content = $('#multid_'+id+'_'+i).val();
             content += '<div id="'+div+'_'+MultilangPlugin.langs[i]+'" style="display:none">'+$('#multid_'+id+'_'+i).val()+'</div>';
+         }
+      }
       content += "<script>$('#"+div+"').bind('languagechangeevt', function() { var elt = $('#"+div+"').parent().find('.descr'); if(!elt.length) elt = $('#"+div+"'); var contentdiv = $('#"+div+"_'+jGallery.lang); if(!contentdiv.length) contentdiv = $('#"+div+"').next();  elt.html(contentdiv.html()); }); $('#"+div+"').trigger('languagechangeevt');</script>";
 
-      $('#d_'+id).val(content); // change the descr textarea with our custom code
+      if(nb_non_empty > 1) {
+         $('#d_'+id).val(content); // change the descr textarea with our custom code
+      } else {
+         $('#d_'+id).val(first_content); // use the only set language
+      }
       cb(); 
    },
  
@@ -53,9 +64,17 @@ var MultilangPlugin = {
       
       var uid = MultilangPlugin.uid(id);
       var old_content = $('<div></div>');
+      var was_multi = false;
       old_content.append($('#d_'+id).val());
+      if(old_content.find('.customtranslate').length)
+         was_multi = true;
       for(var i in MultilangPlugin.langs) {
          var lang_content = old_content.find('#'+uid+'_content_'+MultilangPlugin.langs[i]).html();
+         if(!was_multi) {
+            lang_content = (i==0)?$('#d_'+id).val():'';
+         } else {
+            lang_content = old_content.find('#'+uid+'_content_'+MultilangPlugin.langs[i]).html();
+         }
 			$("#multilangTpl").tmpl({id:id,i:i,content:lang_content}).appendTo($('#d_'+id).parent());
 			$("#multilangTplFlag").tmpl({id:id,i:i,lang:MultilangPlugin.langs[i]}).appendTo($('#flags_'+id));
          $("#flag_"+id+"_"+i).click({index:i}, function(e) {
