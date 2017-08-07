29b393453eff75bb111b387f10d04f5800dd216e
BLepers <baptiste.lepers@gmail.com>
2017-08-07 10:01:48 +0200

    UTF-8

--- scripts/lang/option_fr.js
+++ scripts/lang/option_fr.js
@@ -1,6 +1,6 @@
 known_sentences.concat({
-   'Multi Lang':'Descriptions Multi-langues<br/><p style="font-size:12px;font-style:normal">Permet d\'écrire les descriptions dans plusieurs langues. La langue affichée sera la même que la langue choisie par l\'utilisateur.</p>',
+   'Multi Lang':'Descriptions Multi-langues<br/><p style="font-size:12px;font-style:normal">Permet d\'Ă©crire les descriptions dans plusieurs langues. La langue affichĂ©e sera la mĂȘme que la langue choisie par l\'utilisateur.</p>',
    'multilang_activated':'Activate plugin',
-   'multilang_langs':'Langues (s�par�s par une virgule, sans espace - e.g., en,fr)',
+   'multilang_langs':'Langues (séparés par une virgule, sans espace - e.g., en,fr)',
 });
 
